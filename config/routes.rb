1f346e85a36aa601f374f74cab0a9939c154baff
James Mitchell <jmitchell238@gmail.com>
2021-04-28 10:15:58 -0500

    Lesson 27 Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :users
+
   root 'events#index'
 
   resources :events do
     resources :registrations
   end
+
+  resources :users
+  get "signup" => "users#new"
+
 
   # get "events/:event_id/registrations", to: "registrations#index"
 
