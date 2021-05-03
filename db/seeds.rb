2ad46002950ea16a1205144572d4df138d862edc
James Mitchell <jmitchell238@gmail.com>
2021-05-03 15:52:33 -0500

    Modified seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,70 +12,70 @@ Event.create!([
                   location: 'Denver, CO',
                   price: 0.00,
                   starts_at: 30.days.from_now,
-                  description: %(
+                  description: %{
       Join us for a fun evening of bug smashing! It's a great
       way to get involved in open source projects whether you're
       reporting bugs, fixing bugs, or even creating
       a few bugs!
-    ).squish
+    }.squish
                 },
                 {
                   name: 'Hackathon',
                   location: 'Austin, TX',
                   price: 15.00,
                   starts_at: 45.days.from_now,
-                  description: %(
+                  description: %{
       Got a killer app idea you've been itching to work on? Hunker
       down and hack away! This is an intense, focused day of hacking
       on anything you want. The entry fee goes toward a bounty of cash
       and prizes for winners in a variety of categories.
-    ).squish
+    }.squish
                 },
                 {
                   name: 'Kata Camp',
                   location: 'Dallas, TX',
                   price: 75.00,
                   starts_at: 65.days.from_now,
-                  description: %(
+                  description: %{
       Kata Camp is where developers go to practice their craft without interruptions. Skip the status reports and stand-up meetings of a typical project. Just get 'er done! Price includes a buffet lunch and a leather-bound journal to record your kata achievements.
-    ).squish
+    }.squish
                 },
                 {
                   name: "Coffee 'n Code",
                   location: 'Portland, OR',
                   price: 0.00,
                   starts_at: 20.days.ago,
-                  description: %(
+                  description: %{
       Start your day off right with a good cup of Joe while you sling some code with other local developers. By the time you hit the office, you'll be in the groove!
-    ).squish
+    }.squish
                 },
                 {
                   name: 'Rails User Group',
                   location: 'Reston, VA',
                   price: 0.00,
                   starts_at: 2.days.ago,
-                  description: %(
+                  description: %{
       Come enjoy a technical talk and meet local Rails developers! This week's topic is a comparison of editors, templating systems, and whether to use tabs or spaces.
-    ).squish
+    }.squish
                 },
                 {
                   name: 'Ruby User Group',
                   location: 'Chigaco, IL',
                   price: 0.00,
                   starts_at: 5.days.ago,
-                  description: %(
+                  description: %{
       Do you heart Ruby? So do we! Every week a local developer presents
       a new Ruby-related topic to help you keep on top of your game.
-    ).squish
+    }.squish
                 },
                 {
                   name: '5-Minute Lightning Talks',
                   location: 'Kansas City, MO',
                   price: 15.00,
                   starts_at: 10.days.ago,
-                  description: %(
+                  description: %{
       Got a newfangled trick? A handy new tool? A just-released library? Here's your chance to share it! But you only get 5 minutes to present your must-know tip, so keep it snappy. Price includes an open ice cream and root beer float bar.
-    ).squish
+    }.squish
                 },
                 {
                   name: 'Drone Zone',
@@ -92,8 +92,9 @@ Event.create!([
                   location: 'San Diego, CA',
                   price: 25.00,
                   starts_at: 10.days.from_now,
-                  description: %(
+                  description: %{
       Why ninjas? We have no idea, but the icon is cool. Don't miss this opportunity to show off your ninja moves.
-    ).squish
+    }.squish
                 }
               ])
+
